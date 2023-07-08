e5c2fb8b93be000d501a55511d647cb6b0ecaa20
Sonic Liu <liuhanyang0730@gmail.com>
2023-07-08 09:33:23 +0800

    Initial Commit

---
+++ YCForum/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  YCForum
+//
+//  Created by Sonic Liu on 8/7/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ YCForum/YCForumApp.swift
@@ -0,0 +1,17 @@
+//
+//  YCForumApp.swift
+//  YCForum
+//
+//  Created by Sonic Liu on 8/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct YCForumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
