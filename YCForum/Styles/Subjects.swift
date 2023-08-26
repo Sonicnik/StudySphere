38dd7430ae5a8bce5956137724a6c104cb0ff812
Sonic Liu <liuhanyang0730@gmail.com>
2023-08-26 17:37:29 +0800

    Created the subject swift file for choosing different subjects

---
+++ YCForum/Styles/Subjects.swift
@@ -0,0 +1,14 @@
+//
+//  Subjects.swift
+//  YCForum
+//
+//  Created by Sonic Liu on 26/8/2023.
+//
+
+import SwiftUI
+
+enum Subject: String {
+    case MATH
+    case Chinese
+    case English
+}
