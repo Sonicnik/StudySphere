222e8700f7916108764e94d61c9b4c0d77f84f40
Sonic Liu <liuhanyang0730@gmail.com>
2023-11-21 19:56:28 +0800

    Biopage added with github link.

---
+++ YCForum/Views/SettingPageView/BIO/BioView.swift
@@ -0,0 +1,18 @@
+//
+//  BioView.swift
+//  YCForum
+//
+//  Created by Sonic Liu on 21/11/2023.
+//
+
+import SwiftUI
+
+struct BioView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BioView()
+}
