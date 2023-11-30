2ec0a474a71fcc43d4f6e405dc8ab39f7de8a6b0
Sonic Liu <liuhanyang0730@gmail.com>
2023-11-30 14:10:06 +0800

    ABC

---
+++ YCForum/Views/InAppNotification/choiceOfSubject.swift
@@ -0,0 +1,18 @@
+//
+//  choiceOfSubject.swift
+//  YCForum
+//
+//  Created by Sonic Liu on 30/11/2023.
+//
+
+import SwiftUI
+
+struct choiceOfSubject: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    choiceOfSubject()
+}
