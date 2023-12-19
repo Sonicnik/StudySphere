f31c1fccc11fe7d88e3075708ff72fe547765a5e
Sonic Liu <liuhanyang0730@gmail.com>
2023-12-19 12:38:20 +0800

    Changes for into page

---
+++ YCForum/Views/SettingPageView/BIO/askReview.swift
@@ -0,0 +1,18 @@
+//
+//  askReview.swift
+//  YCForum
+//
+//  Created by Sonic Liu on 14/12/2023.
+//
+
+import SwiftUI
+
+struct askReview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    askReview()
+}
